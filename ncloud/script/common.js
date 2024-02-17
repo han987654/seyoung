58e1d384161431f99957c01e523ec31a24b31237
a85100831a <a97290831a@gmail.com>
2024-02-18 02:28:43 +0900

    반응형

--- ncloud/script/common.js
+++ ncloud/script/common.js
@@ -36,24 +36,66 @@ $(function(){
 
 	}).trigger('scroll');
 
+    
+    $('.gnb>li').hover(function () {
+        $(this).addClass('current').siblings('li').removeClass('current');
 
-        //-----메뉴
-        $('.gnb>li').hover(function(){
-            $(this).addClass('current').siblings('li').removeClass('current');
-            $('.depth2_list>li').removeClass('hover');
-            $('.depth2_list>li.current').show();
-            // $('.depth2_title>li').css({color:'red'})
-        }, function(){
-            $('.gnb>li').removeClass('current');
-        });
+    }, function () {
+        $('.depth2_title>li').eq(0).addClass('current').siblings('li').removeClass('current');
+        $('.depth2_list>li').eq(0).addClass('current').siblings('li').removeClass('current');
+        $('.gnb>li').removeClass('current');
+    });
+    
+    $('.depth2_title>li').hover(function(){
+        var n = $(this).index();
+        $(this).addClass('current').siblings('li').removeClass('current');
         
-        $('.depth2_title>li').hover(function(){
-            $('.depth2_list>li.current').hide();
-            $('.depth2_list>li').eq($(this).index()).addClass('hover').siblings('li').removeClass('hover');
-        },function(){
-
-        });
-})
+        $('.depth2_list>li').eq(n).addClass('current').siblings('li').removeClass('current');
+    });
+    
+    $('.js-gnb-open').click(function(){
+        $('body').addClass('gnb-open');
+    });
+    
+    $('.js-gnb-cls').click(function(){
+        $('body').removeClass('gnb-open');
+    });
+    
+    var mobileGnbToogle = function( $this, toogleList, toogleParent, toogleParentList){
+        $this.next(toogleList).slideToggle().parents(toogleParent).siblings(toogleParentList).find(toogleList).slideUp();
+        $this.parents(toogleParent).addClass('depth-open').siblings(toogleParentList).removeClass('depth-open'); //+ - 버튼 css 할거임.
+    }
+    $('.gnb>li>a').click(function(){
+        var $this = $(this),
+            toogleList = $('.depth'),
+            toogleParent = $('.gnb>li'),
+            toogleParentList = $('li')
+        mobileGnbToogle( $this, toogleList, toogleParent, toogleParentList );
+        return false;
+    });
+    
+    $('.depth2_list >li >h2').click(function(){
+        
+        var $this = $(this),
+            toogleList = $('.depth2_sub_list'),
+            toogleParent = $('.depth2_list>li'),
+            toogleParentList = $('li')
+        mobileGnbToogle( $this, toogleList, toogleParent, toogleParentList );
+        
+    });
+    
+    $('.depth_list>li>span').click(function(){
+        
+         var $this = $(this),
+            toogleList = $('.depth_right_list'),
+            toogleParent = $('.depth_list'),
+            toogleParentList = $('ul')
+        mobileGnbToogle( $this, toogleList, toogleParent, toogleParentList );
+        
+    });
+    
+   
+});
 
 
 
